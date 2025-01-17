c188ae6d887bca35e1e8d624cea02b6433289ef4
Morgan Browne <morgan.browne@keyin.com>
2025-01-17 00:02:38 -0330

    Finished Assign #1. Started Assign #2

--- src/AverageTemperature.java
+++ src/AverageTemperature.java
@@ -1,3 +1,5 @@
+// Assignment #1: Average Temperature.
+
 import java.util.Scanner;
 
 public class AverageTemperature {
---
+++ src/MovieTheater.java
@@ -0,0 +1,20 @@
+/**
+ * Assignment 2: Movie Theater Implementation
+ 1: Simulate a movie theater that helps users to book and reserve seats.
+ 2: reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+cancel seats
+ 3: retrieve initial seating charting
+ **/
+
+public class MovieTheater {
+    // Make 2D array "seats".
+    private int[][] seats;
+
+    public MovieTheater (int rows, int columns ){
+        seats = new int[rows][columns];
+    }
+
+    public boolean reserveSeat (int rows, int column) {
+
+    }
+}
