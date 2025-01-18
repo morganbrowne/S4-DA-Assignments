6bd227ea930f1a5c66783e68d47c9490e4de0d64
Morgan Browne <morgan.browne@keyin.com>
2025-01-18 00:38:02 -0330

    Added cancelSeat method to Assignment 2.

--- src/MovieTheater.java
+++ src/MovieTheater.java
@@ -8,13 +8,61 @@ cancel seats
 
 public class MovieTheater {
     // Make 2D array "seats".
-    private int[][] seats;
+    private final int[][] seats;
 
     public MovieTheater (int rows, int columns ){
         seats = new int[rows][columns];
     }
 
-    public boolean reserveSeat (int rows, int column) {
+    public boolean reserveSeat (int row, int column) {
+        if (seats[row][column] == 1 ) {
+            System.out.println("This seat is taken, please choose different seat. ");
+            return false;
+        } else { // If seat is open assign to user
+            seats [row] [column] = 1;
+            System.out.println("Seat Reserved");
+            return true;
+        }
+    }
+
+    public boolean cancelSeat(int row, int column) {
+        if (seats[row][column] == 0) {
+            System.out.println("This seat is already empty.");
+            return false;
+        } else {
+            seats[row][column] = 0;
+            System.out.println("Seat reservation canceled.");
+            return true;
+        }
+    }
+
+    public void printSeating() {
+        System.out.println("Seating Chart: ");
+        for (int[] seat : seats) {
+            for (int i : seat) {
+                System.out.print(i == 1 ? "[x]" : "[ ]");
+            }
+            System.out.println();
+        }
+    }
+
+    // Testing to see if code functions right.
+    public static void main(String[] args) {
+        MovieTheater theater = new MovieTheater(25,25);
+        theater.printSeating();
+
+        if (theater.reserveSeat(18,12)) {
+            System.out.println("Seat '18.12'Reserved ");
+        } else {
+            System.out.println("Failed to reserve seat 4.8");
+        }
+        theater.reserveSeat(2,3);
+        theater.reserveSeat(7,9);
+        theater.reserveSeat(5,4);
+
+        theater.printSeating();
+
 
     }
 }
+
