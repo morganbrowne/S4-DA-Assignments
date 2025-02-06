5b173040f91adeee3d5a6861915b0f13edb7b022
Morgan Browne <morgan.browne@keyin.com>
2025-02-06 15:55:01 -0330

    Finished Animal Class for Assignment #5

--- src/Assignments/Queues/Animal.java
+++ src/Assignments/Queues/Animal.java
@@ -1,3 +1,11 @@
+/**
+ * Assignment #5:
+ * An Animal shelter, which holds only dogs and cats, operates on a strictly "first in, first out" bases.
+ * People must adopt either the "oldest" (based on arrival time) of  all animals at the shelter, or they can select
+ *  whether they would prefer a dog or cat (and will receive the oldest animal of that type).
+ *  They cannot select which specific animal they would like.
+ *  Create a datastucture to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog and DequeueCat.
+ */
 package Assignments.Queues;
 
 
@@ -12,5 +20,18 @@ public class Animal extends AnimalShelterManager {
         this.type = type;
         this.orderOfArival = orderOfArival;
     }
+
+    public String getName() {
+        return name;
+    }
+    public String geType() {
+        return type;
+    }
+    public String getOrderOfArival() {
+        return orderOfArival;
+    }
+
+
+
 }
 
--- src/Assignments/Queues/AnimalShelterManager.java
+++ src/Assignments/Queues/AnimalShelterManager.java
@@ -1,9 +1,5 @@
-package Assignments.Queues;
-
-import java.util.Queue;
-
 /**
- * Assignment #4:
+ * Assignment #5:
  * An Animal shelter, which holds only dogs and cats, operates on a strictly "first in, first out" bases.
  * People must adopt either the "oldest" (based on arrival time) of  all animals at the shelter, or they can select
  *  whether they would prefer a dog or cat (and will receive the oldest animal of that type).
@@ -11,11 +7,33 @@ import java.util.Queue;
  *  Create a datastucture to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog and DequeueCat.
  */
 
+package Assignments.Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 //
 public class AnimalShelterManager {
     private Queue<Animal> dog;
     private Queue<Animal> cat;
     private int orderCount;
+
+    //  Now have to Initialize the two animal queues into linked lists.
+    //dog and cat.
+    public AnimalShelterManager() {
+        dog = new LinkedList<>();
+        cat = new LinkedList<>();
+        orderCount = 0;
+    }
+    // enQueue to add animal to the shelter.
+    public void enQueue() {
+
+
+    }
+
+
+
+
 
 
 
